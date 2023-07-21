d17f2eb7b70ee378fe503148a922702615cf6d04
Sebastian Cyde <sebastian.cyde@red-engine.com>
2023-07-21 13:49:28 +0100

    fixed work converter, needs logs

--- index.js
+++ index.js
@@ -1,15 +1,6 @@
-const fs = require("fs").promises;
-const { FileFisher } = require("./FileFisher");
-const { GetInput } = require("./GetInput");
-const { PDFCopier } = require("./PDFCopier");
-const { HTMLCopier } = require("./HTMLCopier");
-const { ExcelCreator } = require("./ExcelCreator");
-const { SubDirectoryCreator } = require("./SubdirectoryCreator");
-const { DataTransfer } = require("./DataTransfer");
-const { GroupFiles } = require("./GroupFiles");
-
-let DirtyDirectory = "C://Users/SebCy/Documents/Documents/Work/Katalon_Dirty";
-let CleanDirectory = "C://Users/SebCy/Documents/Documents/Work/Katalon_Clean";
+const { GetInput } = require("./Functions/GetInput");
+const { HomeConverter } = require("./HomeConverter");
+const { WorkConverter } = require("./WorkConverter");
 
 const currentDate = new Date();
 const day = String(currentDate.getUTCDate()).padStart(2, "0");
@@ -17,107 +8,22 @@ const month = String(currentDate.getUTCMonth() + 1).padStart(2, "0");
 const year = currentDate.getUTCFullYear();
 const formattedDate = `${day}${month}${year}`;
 
-let LogDirectory = `C://Users/SebCy/Documents/Documents/Work/Katalon_Cleaner_Logs`;
-
 const Main = async () => {
   const location = await GetInput("Work or Home? ");
-  if (location.toLowerCase() == "work") {
-    DirtyDirectory =
-      "C:/Users/sebastian.cyde/Documents/Other/Katalon/UK Site Tests/flightclub-testing/Reports";
-    CleanDirectory = "C:/Users/sebastian.cyde/Documents/Other/KatReports";
-    LogDirectory = `C:/Users/sebastian.cyde/Documents/Other/CleanerLogs/`;
-  }
 
-  let LogPath = `${LogDirectory}/${formattedDate}.txt`;
-
-  const files = await fs.readdir(DirtyDirectory, { withFileTypes: true });
-  const DirtyDirectories = files
-    .filter((file) => file.isDirectory())
-    .map((file) => file.name)
-    .filter((name) => name.toLowerCase() != "self-healing");
-
-  console.log("Creating clean container");
-  const NewCleanDirectory = `${CleanDirectory}/${formattedDate}`;
-
-  try {
-    await fs.access(NewCleanDirectory);
-    console.log("Clean directory already exists.");
-  } catch (error) {
-    await fs.mkdir(NewCleanDirectory);
-    console.log("Created clean container");
-  }
-
-  async function createLogFile() {
-    try {
-      await fs.access(LogDirectory);
-      console.log("Log directory already exists.");
-    } catch (error) {
-      try {
-        await fs.mkdir(LogDirectory, { recursive: true });
-        console.log("Log directory created successfully.");
-      } catch (error) {
-        console.error("Error creating log directory:", error);
-        return; // Abort further processing if directory creation fails
-      }
-    }
-
-    try {
-      await fs.writeFile(LogPath, "");
-      console.log("Log file created successfully.");
-    } catch (error) {
-      console.error("Error creating log file:", error);
-    }
-  }
-
-  await createLogFile();
-  await SubDirectoryCreator(NewCleanDirectory);
-  fs.appendFile(LogPath, `Date of execution: ${formattedDate}\n\n`);
-
-  let Res = {
-    Total: 0,
-    Passed: 0,
-    Failed: 0,
-    Skipped: 0,
-  };
-
-  console.log("Top Level Directories:", DirtyDirectories);
-  let transferredDirectories = 0;
-  const DirectoriesToClean = [];
-  let failedTests = [];
-
-  // TODO - FIX FROM HERE
-
-  // Remove all nested directories that have files in them into DirectoriesToClean
-  const TransferDirectories = async () => {
-    for (const Dir of DirtyDirectories) {
-      console.log("Currently transferring:", Dir);
-      try {
-        // Get files from all directories in each top level directory
-        const AllFileDetails = await FileFisher(`${DirtyDirectory}/${Dir}`);
-        console.log("All File Details:", AllFileDetails.length);
-
-        AllFileDetails.forEach((FileDetails) => {
-          // console.log("FD:", FileDetails);
-          DirectoriesToClean.push(FileDetails);
-        });
-        transferredDirectories++;
-      } catch (error) {
-        console.error("Error transferring directory from top level directory:", error);
-      }
-    }
-  };
-
-  await TransferDirectories();
-  const GroupedFiles = await GroupFiles(DirectoriesToClean);
-
-  console.log(" ");
-  console.log("Directories to clean:", DirectoriesToClean.length);
-  console.log("Grouped Files:", GroupedFiles.length);
-
-  GroupedFiles.forEach((Group) => {
-    console.log(Group);
-    console.log(" ");
-  });
+  location.toLowerCase() == "work"
+    ? await WorkConverter(
+        "C:/Users/sebastian.cyde/Documents/Other/Katalon/UK Site Tests/flightclub-testing/Reports",
+        "C:/Users/sebastian.cyde/Documents/Other/KatReports",
+        `C:/Users/sebastian.cyde/Documents/Other/CleanerLogs`,
+        formattedDate
+      )
+    : await HomeConverter(
+        "C://Users/SebCy/Documents/Documents/Work/Katalon_Dirty",
+        "C://Users/SebCy/Documents/Documents/Work/Katalon_Clean",
+        `C://Users/SebCy/Documents/Documents/Work/Katalon_Cleaner_Logs`,
+        formattedDate
+      );
 
   // Clean and convert the transferred directories
   // DirectoriesToClean.forEach((dir) => {
@@ -225,26 +131,6 @@ const Main = async () => {
   // });
 
   // Log stuff
-  console.log(`Conversions complete: ${Res.Total}/${Res.Total}`);
-  console.log(`Skipped: ${Res.Skipped}`);
-  console.log(`Passed: ${Res.Passed}`);
-  console.log(`Failed: ${Res.Failed}`);
-  console.log(" ");
-
-  console.log("Updating Logs..");
-
-  fs.appendFile(LogPath, "\n");
-  fs.appendFile(LogPath, `Conversions complete: ${Res.Total}/${Res.Total}\n`);
-  fs.appendFile(LogPath, `Skipped: ${Res.Skipped}\n`);
-  fs.appendFile(LogPath, `Passed: ${Res.Passed}\n`);
-  fs.appendFile(LogPath, `Failed: ${Res.Failed}\n`);
-  fs.appendFile(LogPath, "\n");
-
-  failedTests.forEach((log) => {
-    fs.appendFile(LogPath, `${log}\n`);
-  });
-  console.log("Logs Updated.");
-  console.log("");
 };
 
 Main();
